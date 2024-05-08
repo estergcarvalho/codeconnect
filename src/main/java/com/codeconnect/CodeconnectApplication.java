37ebcdc4ffe150e64392cda6c4219b1e9566bd2c
estergcarvalho <ester.estudosti@gmail.com>
2024-05-08 15:43:56 -0300

    feat: Criacao de estrutura inicial

---
+++ src/main/java/com/codeconnect/CodeconnectApplication.java
@@ -0,0 +1,13 @@
+package com.codeconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeconnect/CodeconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
