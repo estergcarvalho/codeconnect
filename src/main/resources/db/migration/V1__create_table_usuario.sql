364b40f3e7989801916907a515d4942f425e66f8
estergcarvalho <ester.estudosti@gmail.com>
2024-05-21 14:13:14 -0300

    refactor: Atualizando nome id tabela banco

--- src/main/resources/db/migration/V1__create_table_usuario.sql
+++ src/main/resources/db/migration/V1__create_table_usuario.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS usuario (
-    usuario_id  SERIAL PRIMARY KEY NOT NULL,
+    id          SERIAL PRIMARY KEY NOT NULL,
     nome        VARCHAR(50) NOT NULL,
     email       VARCHAR(100) NOT NULL,
     senha       VARCHAR(255) NOT NULL
