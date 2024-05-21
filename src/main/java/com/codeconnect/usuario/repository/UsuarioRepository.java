c0b7d8cb386d1d06501bfe0b0dca27a2976056b6
Ester Gomes <129803263+estergcarvalho@users.noreply.github.com>
2024-05-21 14:23:46 -0300

    feat: Criacao de classe repository usuario (#16)

---
+++ src/main/java/com/codeconnect/usuario/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.codeconnect.usuario.repository;
+
+import com.codeconnect.usuario.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+}
