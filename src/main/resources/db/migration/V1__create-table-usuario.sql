35041b536a41ea96b598c70eab0eebf30d4e8786
estergcarvalho <ester.estudosti@gmail.com>
2024-05-21 14:00:06 -0300

    refactor: Correcao do nome tabela usuario

--- src/main/resources/db/migration/V1__create-table-usuario.sql
+++ src/main/resources/db/migration/V1__create-table-usuario.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS usuarios (
+CREATE TABLE IF NOT EXISTS usuario (
     usuario_id  SERIAL PRIMARY KEY NOT NULL,
     nome        VARCHAR(50) NOT NULL,
     email       VARCHAR(100) NOT NULL,
