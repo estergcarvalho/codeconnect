d2877d5faa2d0ecfc52221e4efc6448e15d3baf8
Ester Gomes <129803263+estergcarvalho@users.noreply.github.com>
2024-05-22 10:32:19 -0300

    feat: Criacao de classe UsuarioDetailsServiceImpl (#21)
    * feat: Criacao de classe UsuarioDetailsServiceImpl
    
    * refactor: Atualiazao de versao java e correcao classe UsuarioDetailsServiceImpl

---
+++ src/main/java/com/codeconnect/security/UsuarioDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.codeconnect.security;
+
+import com.codeconnect.usuario.exception.UsuarioNaoEncontradoException;
+import com.codeconnect.usuario.model.Usuario;
+import com.codeconnect.usuario.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(username)
+            .orElseThrow(UsuarioNaoEncontradoException::new);
+
+        return new UsuarioDetailsImpl(usuario);
+    }
+
+}
