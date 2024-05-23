55027e97fd738ebe04f71a789fd716cbc212e820
Ester Gomes <129803263+estergcarvalho@users.noreply.github.com>
2024-05-23 15:36:46 -0300

    feat: Criacao de classe SecurityConfig (#24)
    * feat: Criacao de classe SecurityConfig
    
    * refactor: Correcao de nome metodo SecutiryFilter e edentacao
    
    * refactor: Correcao de edentacao
    
    * refactor: Correcao de espaco

---
+++ src/main/java/com/codeconnect/security/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.codeconnect.security;
+
+import com.codeconnect.security.exception.ErroConfiguracaoSegurancaException;
+import com.codeconnect.security.exception.ErroGerenciamentoDeAutenticaoException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class SecurityConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
+
+    @Bean
+    public SecurityFilterChain filtroDeSegurancaChain(HttpSecurity httpSecurity) {
+        try {
+            log.info("Iniciando configuracao da cadeia de filtros de segurança");
+
+            SecurityFilterChain filtroDeSeguranca = httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(configuracaoSessao -> configuracaoSessao.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(autorizacaoRequisicoes -> {
+                    autorizacaoRequisicoes.requestMatchers("/login").permitAll();
+                })
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+            log.info("Cadeia de filtros de segurança configurada com sucesso");
+
+            return filtroDeSeguranca;
+        } catch (Exception e) {
+            throw new ErroConfiguracaoSegurancaException();
+        }
+    }
+
+    @Bean
+    public AuthenticationManager gerenciadorDeAutenticacao(AuthenticationConfiguration configuracao) {
+        try {
+            return configuracao.getAuthenticationManager();
+        } catch (Exception e) {
+            throw new ErroGerenciamentoDeAutenticaoException();
+        }
+    }
+
+    @Bean
+    public PasswordEncoder codificadorDeSenha() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- src/main/java/com/codeconnect/security/SecurityFilter.java
+++ src/main/java/com/codeconnect/security/SecurityFilter.java
@@ -6,19 +6,18 @@ import com.codeconnect.usuario.exception.UsuarioNaoEncontradoException;
 import com.codeconnect.usuario.model.Usuario;
 import com.codeconnect.usuario.repository.UsuarioRepository;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-
+@Component
 @Slf4j
-public class SecurityFIlter extends OncePerRequestFilter {
+public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private TokenService tokenService;
@@ -27,7 +26,7 @@ public class SecurityFIlter extends OncePerRequestFilter {
     private UsuarioRepository usuarioRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
         try {
             log.info("Inicio filtrar requisicao");
 
@@ -45,11 +44,11 @@ public class SecurityFIlter extends OncePerRequestFilter {
 
                 log.info("Autenticação realizada com sucesso");
             }
+            
             filterChain.doFilter(request, response);
         } catch (Exception exception) {
             throw new ErroAoAutenticarUsuarioException();
         }
-
     }
 
     private String recuperarToken(HttpServletRequest request) {
@@ -57,13 +56,12 @@ public class SecurityFIlter extends OncePerRequestFilter {
             var autorizacao = request.getHeader("Authorization");
 
             if (autorizacao != null) {
-                return autorizacao.replace("Bearer", "");
+                return autorizacao.replace("Bearer ", "");
             }
 
             log.info("Cabeçalho 'Authorization' não encontrado ou vazio.");
 
             return null;
-
         } catch (Exception exception) {
             throw new ErroAoRecuperarTokenExpection();
         }
