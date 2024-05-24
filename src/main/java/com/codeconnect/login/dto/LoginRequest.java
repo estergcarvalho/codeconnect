5b296e0383006b1a24739d10379cdeaf9995dc2d
estergcarvalho <ester.estudosti@gmail.com>
2024-05-23 22:18:06 -0300

    refactor: Adicionando validacao loginRequest

--- src/main/java/com/codeconnect/login/dto/LoginRequest.java
+++ src/main/java/com/codeconnect/login/dto/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.codeconnect.login.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +12,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoginRequest {
 
+    @NotBlank(message = "E-mail do usuario não deve ser nulo ou vazio")
     private String email;
+
+    @NotBlank(message = "Senha do usuario não deve ser nula ou vazia")
     private String senha;
 
 }
