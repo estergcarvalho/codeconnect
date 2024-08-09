fe1ba00113f80171b7a3f1b16b9717b2447c82b3
estergcarvalho <ester.estudosti@gmail.com>
2024-08-09 19:22:32 -0300

    refactor: Exclusao de import nao utilizado no PostComentario

--- src/main/java/com/codeconnect/post/model/PostComentario.java
+++ src/main/java/com/codeconnect/post/model/PostComentario.java
@@ -1,7 +1,6 @@
 package com.codeconnect.post.model;
 
 import com.codeconnect.usuario.model.Usuario;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
